672e3ef033dbb74574c29e49a708729e90129565
Enaric <1781183680@qq.com>
2016-08-13 23:26:56 +0800

    post biz层

---
+++ src/main/java/web/biz/impl/PostManage.java
@@ -0,0 +1,29 @@
+package web.biz.impl;
+
+import web.biz.IPostManage;
+import web.model.communication.Post;
+import web.model.communication.PostBasicInfo;
+import web.model.communication.PostContent;
+import web.model.communication.PostViewAttitude;
+
+/**
+ * Created by alfred on 16/8/13.
+ */
+public class PostManage implements IPostManage{
+    //TODO
+    public boolean publish(PostBasicInfo basicInfo, PostContent content) {
+        return false;
+    }
+
+    public void delete(int id) {
+
+    }
+
+    public void attitude(int id, PostViewAttitude attitude) {
+
+    }
+
+    public Post search(String str) {
+        return null;
+    }
+}
